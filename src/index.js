e49b08c478c2dc4d7beb75c49ef51893d16e0928
williamNyongesa <marakawilliam@gmail.com>
2023-07-21 12:41:38 +0300

    completed mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let input = document.getElementById("new-task-description");
+    let list = document.getElementById("tasks");
+    let listElements = document.createElement("li");
+    listElements.innerText = input.value;
+
+    // let deleteButton = document.createElement("button");
+    // deleteButton.innerText = "Delete";
+    // deleteButton.addEventListener("click", function () {
+    //   list.removeChild(listElements);
+    // });
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "DELETE"
+    deleteButton.addEventListener("click",function(event){
+      event.preventDefault();
+      
+      list.removeChild(listElements);
+    })
+
+
+
+    listElements.appendChild(deleteButton); // Add the delete button to the task item
+    list.appendChild(listElements);
+    input.value = ""; // Clear the input field after adding the task
+  });
 });
